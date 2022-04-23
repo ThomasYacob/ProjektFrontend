d0c195e0198f6699418df29c00531896d08a2633
ThomasYacob <thomas.yacob@hotmail.com>
2022-04-23 14:46:40 +0200

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { User } from './user';
 import { TokenStorageService } from './_services/token-storage.service';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,16 @@ import { TokenStorageService } from './_services/token-storage.service';
 })
 export class AppComponent {
 
-  userModel = new User('', 'hejsan', 'rob@test.com')
+  userModel = new User('robert', 'hejsan1s', 'rob@test.com')
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  onSubmit() {
+    this._enrollmentService.enroll(this.userModel)
+        .subscribe(
+            data => console.log('Success!', data),
+            error => console.error('Error!', error)
+        )
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-// @ts-ignore
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
