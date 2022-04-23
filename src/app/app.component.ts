c17d1ff9da4edb63341bae6b787149a1008306dc
ThomasYacob <thomas.yacob@hotmail.com>
2022-04-23 13:25:41 +0200

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { User } from './user';
 import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
@@ -7,24 +8,6 @@ import { TokenStorageService } from './_services/token-storage.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private roles: string[] = [];
-  isLoggedIn = false;
-  showAdminBoard = false;
-  showModeratorBoard = false;
-  username?: string;
-  constructor(private tokenStorageService: TokenStorageService) { }
-  ngOnInit(): void {
-    this.isLoggedIn = !!this.tokenStorageService.getToken();
-    if (this.isLoggedIn) {
-      const user = this.tokenStorageService.getUser();
-      this.roles = user.roles;
-      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
-      this.username = user.username;
-    }
-  }
-  logout(): void {
-    this.tokenStorageService.signOut();
-    window.location.reload();
-  }
+
+  userModel = new User('', 'hejsan', 'rob@test.com')
 }
