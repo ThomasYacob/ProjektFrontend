df9374eca2727778b9b8bdd8d6501b57b0ccdfaf
ThomasYacob <thomas.yacob@hotmail.com>
2022-05-03 22:06:15 +0200

    home component timer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
 
 @Component({
@@ -6,8 +6,34 @@ import { UserService } from '../_services/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   content?: string;
+  currentDate: any;
+  targetDate: any;
+  cDateMillisecs: any;
+  tDateMillisecs: any;
+  difference: any;
+  seconds: any;
+  minutes: any;
+  hours: any;
+  days: any;
+  year: number = 2022;
+  month: number = 4;
+  months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'April',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  day: number = 5;
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
@@ -21,7 +47,43 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit() {
+    this.myTimer();
+  }
+
+  myTimer() {
+    this.currentDate = new Date();
+    this.targetDate = new Date(2022, 4, 5);
+    this.cDateMillisecs = this.currentDate.getTime();
+    this.tDateMillisecs = this.targetDate.getTime();
+    this.difference = this.tDateMillisecs - this.cDateMillisecs;
+    this.seconds = Math.floor(this.difference / 1000);
+    this.minutes = Math.floor(this.seconds / 60);
+    this.hours = Math.floor(this.minutes / 60);
+    this.days = Math.floor(this.hours / 24);
+
+    this.hours %= 24;
+    this.minutes %= 60;
+    this.seconds %= 60;
+    this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
+    this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+    this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+
+    // @ts-ignore
+    document.getElementById('days').innerText = this.days;
+    // @ts-ignore
+    document.getElementById('hours').innerText = this.hours;
+    // @ts-ignore
+    document.getElementById('mins').innerText = this.minutes;
+    // @ts-ignore
+    document.getElementById('seconds').innerText = this.seconds;
+
+    setInterval(this.myTimer, 1000);
+  }
+
 }
+
+
 
 // import { Component, OnInit } from '@angular/core';
 // import { UserService } from '../_services/user.service';
