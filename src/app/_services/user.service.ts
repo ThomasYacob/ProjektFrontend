59191a94d99f79a8df8d3e3492486b90b1336935
ThomasYacob <thomas.yacob@hotmail.com>
2022-05-19 19:28:17 +0200

    fixes

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {User} from "../register/user";
+import {catchError} from "rxjs/operators";
 const API_URL = 'http://localhost:8080/api/user/';
 @Injectable({
   providedIn: 'root'
@@ -21,5 +23,33 @@ export class UserService {
     return this.http.get(API_URL + 'admin', { responseType: 'text' });
   }
 
-  
+  getAllUsers() {
+    return this.http.get<User[]>(API_URL + 'all');
+  }
+
+  deleteUser(id: number):Observable<{}> {
+    return this.http.delete('${this.API_URL}/${id}')
+        .pipe(catchError(this.handleError));
+  }
+
+  updateUser(user: User) {
+    return this.http.put('${this.API_URL}/updateuser', user)
+        .pipe(catchError(this.handleError));
+  }
+
+  getUserById(id: number) {
+    return this.http.get<User>('${this.baseUrl}/${id}')
+        .pipe(catchError(this.handleError));
+  }
+
+  private handleError(httpError: HttpErrorResponse) {
+    if(httpError.error instanceof ErrorEvent) {
+      console.error('An error occurred:', httpError.error.message);
+    } else {
+      console.error(
+          `Backend returned code ${httpError.status}, ` +
+          `body was: ${httpError.error}`);
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
 }
--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { UserService } from '../_services/user.service';
 import { EventBusService } from '../_shared/event-bus.service';
 import { EventData } from '../_shared/event.class';
+import {User} from "../register/user";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-board-admin',
@@ -13,17 +15,81 @@ export class BoardAdminComponent implements OnInit {
 
   constructor(private userService: UserService, private eventBusService: EventBusService) { }
 
-  ngOnInit(): void {
-    this.userService.getAdminBoard().subscribe(
-        data => {
-          this.content = data;
-        },
-        err => {
-          this.content = err.error.message || err.error || err.message;
+  // ngOnInit(): void {
+  //   this.userService.getAdminBoard().subscribe(
+  //       data => {
+  //         this.content = data;
+  //       },
+  //       err => {
+  //         this.content = err.error.message || err.error || err.message;
+  //
+  //         if (err.status === 403)
+  //           this.eventBusService.emit(new EventData('logout', null));
+  //       }
+  //   );
+  // }
 
-          if (err.status === 403)
-            this.eventBusService.emit(new EventData('logout', null));
-        }
-    );
-  }
+    users: User[] | undefined;
+    user: User | undefined;
+    deleteMsg:string = "";
+    // @ts-ignore
+    @ViewChild('closebutton') closebutton;
+
+    ngOnInit(): void {
+        console.log('All users ')
+        this.userService.getAllUsers().subscribe(data => {
+            console.log(data);
+            this.users = data;
+        })
+    }
+
+    onClickDelete(userId: number) {
+        this.userService.deleteUser(userId)
+            .subscribe(responseData => {
+                this.deleteMsg = 'Successfully deleted';
+                this.userService.getAllUsers().subscribe(data => {
+                    console.log(data);
+                    this.users = data;
+                })
+            }, (error: string) => {
+                this.deleteMsg = error;
+            });
+    }
+
+    userUpdateForm = new FormGroup({
+        username: new FormControl('', [Validators.required, Validators.minLength(5)]),
+        email: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        password: new FormControl('')
+    })
+
+    onClickUpdate(userId: number) {
+        this.userService.getUserById(userId)
+            .subscribe(responseData => {
+                this.user = responseData;
+                console.log(this.user);
+                this.prepareUpdateForm();
+            });
+    }
+
+    prepareUpdateForm() {
+        this.userUpdateForm.setValue({
+            username: this.user?.username,
+            email: this.user?.email,
+            password: this.user?.password
+        });
+    }
+
+    onSubmit() {
+        let user = new User(<number>this.user?.id, <string>this.user?.username, <string>this.user?.email, <string>this.user?.password);
+        user.username = this.userUpdateForm.value.username;
+        user.email = this.userUpdateForm.value.email;
+        user.password = this.userUpdateForm.value.password;
+        this.userService.updateUser(user).subscribe(responseDate => {
+                this.closebutton.nativeElement.click();
+                this.userService.getAllUsers().subscribe(data => {
+                    this.users = data;
+                })
+            },
+            (error: any) => console.log(error));
+    }
 }
