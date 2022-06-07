48b6117899db72015617f818525548d6a1cb00ea
ThomasYacob <thomas.yacob@hotmail.com>
2022-06-07 16:06:23 +0200

    fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,32 +29,32 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   currentUser: any;
   content?: string;
-  currentDate: any;
-  targetDate: any;
-  cDateMillisecs: any;
-  tDateMillisecs: any;
-  difference: any;
-  seconds: any;
-  minutes: any;
-  hours: any;
-  days: any;
-  year: number = 2022;
-  month: number = 5;
-  months: String[] = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'April',
-    'May',
-    'June',
-    'July',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec'
-  ];
-  day: number = 5;
+  // currentDate: any;
+  // targetDate: any;
+  // cDateMillisecs: any;
+  // tDateMillisecs: any;
+  // difference: any;
+  // seconds: any;
+  // minutes: any;
+  // hours: any;
+  // days: any;
+  // year: number = 2022;
+  // month: number = 5;
+  // months: String[] = [
+  //   'Jan',
+  //   'Feb',
+  //   'Mar',
+  //   'April',
+  //   'May',
+  //   'June',
+  //   'July',
+  //   'Aug',
+  //   'Sept',
+  //   'Oct',
+  //   'Nov',
+  //   'Dec'
+  // ];
+  // day: number = 5;
   constructor(private userService: UserService, private dailyService: DailyService,private monthlyService: MonthlyService, private weeklyService: WeeklyService, private userAnswerService: UseranswerService) { }
 
   monthlys?: Monthly[];
@@ -126,10 +126,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
       this.weeklys = data;
     })
 
-    
-
-    
-
   }
 
   ngAfterViewInit() {
@@ -138,33 +134,33 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   myTimer() {
 
-    this.currentDate = new Date();
-    this.targetDate = new Date(2022, 5, 5);
-    this.cDateMillisecs = this.currentDate.getTime();
-    this.tDateMillisecs = this.targetDate.getTime();
-    this.difference = this.tDateMillisecs - this.cDateMillisecs;
-    this.seconds = Math.floor(this.difference / 1000);
-    this.minutes = Math.floor(this.seconds / 60);
-    this.hours = Math.floor(this.minutes / 60);
-    this.days = Math.floor(this.hours / 24);
+    // this.currentDate = new Date();
+    // this.targetDate = new Date(2022, 5, 5);
+    // this.cDateMillisecs = this.currentDate.getTime();
+    // this.tDateMillisecs = this.targetDate.getTime();
+    // this.difference = this.tDateMillisecs - this.cDateMillisecs;
+    // this.seconds = Math.floor(this.difference / 1000);
+    // this.minutes = Math.floor(this.seconds / 60);
+    // this.hours = Math.floor(this.minutes / 60);
+    // this.days = Math.floor(this.hours / 24);
+    //
+    // this.hours %= 24;
+    // this.minutes %= 60;
+    // this.seconds %= 60;
+    // this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
+    // this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+    // this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+    //
+    // // @ts-ignore
+    // document.getElementById('days').innerText = this.days;
+    // // @ts-ignore
+    // document.getElementById('hours').innerText = this.hours;
+    // // @ts-ignore
+    // document.getElementById('mins').innerText = this.minutes;
+    // // @ts-ignore
+    // document.getElementById('seconds').innerText = this.seconds;
 
-    this.hours %= 24;
-    this.minutes %= 60;
-    this.seconds %= 60;
-    this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
-    this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
-    this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
-
-    // @ts-ignore
-    document.getElementById('days').innerText = this.days;
-    // @ts-ignore
-    document.getElementById('hours').innerText = this.hours;
-    // @ts-ignore
-    document.getElementById('mins').innerText = this.minutes;
-    // @ts-ignore
-    document.getElementById('seconds').innerText = this.seconds;
-
-    setInterval(this.myTimer, 1000);
+    // setInterval(this.myTimer, 1000);
   }
 
   monthlySubmit(id: number) {
@@ -202,46 +198,3 @@ export class HomeComponent implements OnInit, AfterViewInit {
         
   }
 }
-
-
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { UserService } from '../_services/user.service';
-// import {User} from "../register/user";
-// import {first} from "rxjs";
-//
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.css']
-// })
-//
-// export class HomeComponent implements OnInit {
-//   currentUser: User;
-//   users = [];
-//
-//   constructor(
-//       private authenticationService: AuthenticationService,
-//       private userService: UserService
-//   ) {
-//     this.currentUser = this.authenticationService.currentUserValue;
-//   }
-//
-//   ngOnInit() {
-//     this.loadAllUsers();
-//   }
-//
-//   deleteUser(username: string) {
-//     this.userService.delete(username)
-//         .pipe(first())
-//         .subscribe(() => this.loadAllUsers());
-//   }
-//
-//   private loadAllUsers() {
-//     this.userService.getAll()
-//         .pipe(first())
-//         .subscribe(users => this.users = users);
-//   }
-// }
